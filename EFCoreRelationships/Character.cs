7ca131b2f3963153253a53c6d6d8f7f655a85b9a
muratilgun <muratilgun34@gmail.com>
2022-03-24 00:55:45 +0300

    n:n

--- EFCoreRelationships/Character.cs
+++ EFCoreRelationships/Character.cs
@@ -11,6 +11,6 @@ namespace EFCoreRelationships
         public User User { get; set; }
         public int UserId { get; set; }
         public Weapon Weapon { get; set; }
-
+        public List<Skill> Skills { get; set; }
     }
 }
--- EFCoreRelationships/Controllers/CharacterController.cs
+++ EFCoreRelationships/Controllers/CharacterController.cs
@@ -19,6 +19,7 @@ namespace EFCoreRelationships.Controllers
             var characters = await _context.Characters
                 .Where(c => c.UserId == userId)
                 .Include(c=> c.Weapon)
+                .Include(c=> c.Skills)
                 .ToListAsync();
 
             return characters;
@@ -56,6 +57,20 @@ namespace EFCoreRelationships.Controllers
                 Character = character
             };
             _context.Weapons.Add(newWeapon);
+            await _context.SaveChangesAsync();
+            return character;
+        }
+        [HttpPost("skill")]
+        public async Task<ActionResult<Character>> AddCharacterSkill(AddCharacterSkillDto request)
+        {
+            var character = await _context.Characters.Where(x=> x.Id == request.CharacterId)
+                .Include(c=> c.Skills).FirstOrDefaultAsync();
+            if (character == null)
+                return NotFound();
+            var skill = await _context.Skills.FindAsync(request.SkillId);
+            if (skill == null)
+                return NotFound();
+            character.Skills.Add(skill);
             await _context.SaveChangesAsync();
             return character;
         }
